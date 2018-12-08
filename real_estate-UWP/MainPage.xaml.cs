79f0dfa1a474ba04bedb78d7864cee3878723c99
Dmitry Zhomir <giantdem@mail.ru>
2018-12-08 23:16:27 +0800

    Добавлена тестовая кнопка перехода на экран со вкладками

--- real_estate-UWP/MainPage.xaml.cs
+++ real_estate-UWP/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using real_estate_UWP.Templates;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -13,18 +14,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace real_estate_UWP
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void TestButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(ScreenTemplate));
         }
     }
 }
