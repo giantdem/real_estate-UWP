66edce7ad366e1d3c3f0c6be1ae0679dcd8e50da
Dmitry Zhomir <giantdem@mail.ru>
2018-12-09 20:33:47 +0800

    Боковое меню приобрело законченный вид

--- real_estate-UWP/Templates/ScreenTemplate.xaml.cs
+++ real_estate-UWP/Templates/ScreenTemplate.xaml.cs
@@ -18,6 +18,8 @@ namespace real_estate_UWP.Templates
 {
     public sealed partial class ScreenTemplate : Page
     {
+        public bool preventPaneClose = false;
+
         public ScreenTemplate()
         {
             this.InitializeComponent();
@@ -26,18 +28,72 @@ namespace real_estate_UWP.Templates
         private void HamburgerButton_Click(object sender, RoutedEventArgs e)
         {
             SplitViewe4ka.IsPaneOpen = !SplitViewe4ka.IsPaneOpen;
+            if (!SplitViewe4ka.IsPaneOpen) preventPaneClose = false; else preventPaneClose = true;
         }
 
         private void TopItems_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (TopItems.SelectedIndex == 0) TopItems.SelectedItem.ToString();
-            if (TopItems.SelectedIndex == 1) Debug.WriteLine("А это второй элемент");
+            if (TopItems.SelectedIndex == -1) return;
+            switch ((TopItems.SelectedItem as ListBoxItem).Name)
+            {
+                case "RealEstateItem":
+                    ContentPage.Navigate(typeof(Sections.RealEstate.RealEstateMain));
+                    break;
+                case "ContractsItem":
+                    ContentPage.Navigate(typeof(Sections.Contracts.ContractsMain));
+                    break;
+                case "AdsItem":
+                    ContentPage.Navigate(typeof(Sections.Ads.AdsMain));
+                    break;
+                case "ClientsItem":
+                    ContentPage.Navigate(typeof(Sections.Clients.ClientsMain));
+                    break;
+                case "ReportingItem":
+                    ContentPage.Navigate(typeof(Sections.Reporting.ReportingMain));
+                    break;
+            }
+            if (SplitViewe4ka.DisplayMode == SplitViewDisplayMode.Overlay) SplitViewe4ka.IsPaneOpen = false;
             if (TopItems.SelectedIndex != -1 && BottomItems.SelectedIndex != -1) BottomItems.SelectedIndex = -1;
         }
 
         private void BottomItems_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            if (BottomItems.SelectedIndex == -1) return;
+            switch ((BottomItems.SelectedItem as ListBoxItem).Name)
+            {
+                case "DraftsItem":
+                    ContentPage.Navigate(typeof(Misc.Drafts.DraftsMain));
+                    break;
+                case "EmployeesItem":
+                    ContentPage.Navigate(typeof(Misc.Employees.EmployeesMain));
+                    break;
+                case "SettingsItem":
+                    ContentPage.Navigate(typeof(Misc.Settings.SettingsMain));
+                    break;
+            }
+            if (SplitViewe4ka.DisplayMode == SplitViewDisplayMode.Overlay) SplitViewe4ka.IsPaneOpen = false;
             if (TopItems.SelectedIndex != -1 && BottomItems.SelectedIndex != -1) TopItems.SelectedIndex = -1;
+        }
+
+        private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (Window.Current.Bounds.Width < 601)
+            {
+                HamburgerButton.Visibility = Visibility.Visible;
+                SplitViewe4ka.DisplayMode = SplitViewDisplayMode.Overlay;
+            }
+            else
+            {
+                HamburgerButton.Visibility = Visibility.Collapsed;
+                SplitViewe4ka.DisplayMode = SplitViewDisplayMode.Inline;
+                SplitViewe4ka.IsPaneOpen = true;
+                preventPaneClose = false;
+            }
+        }
+
+        private void SplitViewe4ka_PaneClosing(SplitView sender, SplitViewPaneClosingEventArgs args)
+        {
+            if (preventPaneClose == true) args.Cancel = true;
         }
     }
 }
