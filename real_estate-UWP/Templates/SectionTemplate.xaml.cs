65c54c981f5e7df094a48b748d322a0a1f2cce8a
Dmitry Zhomir <giantdem@mail.ru>
2019-01-29 22:11:13 +0800

    Пересмотрена структура

--- real_estate-UWP/Templates/SectionTemplate.xaml.cs
+++ real_estate-UWP/Templates/SectionTemplate.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -13,18 +14,22 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace real_estate_UWP.Templates
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class SectionTemplate : Page
     {
         public SectionTemplate()
         {
             this.InitializeComponent();
+        }
+
+        private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (ContentContainer.ActualWidth < 601)
+            {
+                ContentArea.Width = ContentContainer.ActualWidth - ContentContainer.Padding.Top * 2;
+            }
+            else ContentArea.Width = 450;
         }
     }
 }
