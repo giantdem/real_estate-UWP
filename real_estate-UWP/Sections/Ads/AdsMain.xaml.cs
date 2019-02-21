60f561d37b75b636d7b60546e7eb0ecb2f0268dc
Dmitry Zhomir <giantdem@mail.ru>
2019-02-21 22:11:17 +0800

    Раздел «Объявления»

--- real_estate-UWP/Sections/Ads/AdsMain.xaml.cs
+++ real_estate-UWP/Sections/Ads/AdsMain.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -13,18 +14,78 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace real_estate_UWP.Sections.Ads
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class AdsMain : Page
     {
         public AdsMain()
         {
             this.InitializeComponent();
+
+            this.ViewModel = new AdsVM();
+        }
+
+        public AdsVM ViewModel { get; set; }
+
+        private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (ContentContainer.ActualWidth < 601)
+            {
+                ContentArea.Width = ContentContainer.ActualWidth - ContentContainer.Padding.Right * 2;
+            }
+            else ContentArea.Width = 450;
+        }
+
+        private void listViewe4ka_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            this.Frame.Navigate(typeof(AdsView));
+        }
+
+        private void filter_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(AdsFilter));
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(AdsAdd));
         }
     }
+
+    public class AdItem
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool? isActive { get; set; }
+
+        public AdItem()
+        {
+            this.Title = "Имя записи";
+            this.Description = "Краткая информация";
+            this.isActive = true;
+        }
+    }
+
+    public class AdsVM
+    {
+        public AdItem DefaultTestItem { get; } = new AdItem();
+        public ObservableCollection<AdItem> Ads { get; } = new ObservableCollection<AdItem>();
+
+        public AdsVM()
+        {
+            this.Ads.Add(new AdItem()
+            {
+                Title = "Объявление 1",
+                Description = "11.12.2019",
+                isActive = true
+            });
+            this.Ads.Add(new AdItem()
+            {
+                Title = "Объявление 2",
+                Description = "12.12.2019",
+                isActive = false
+            });
+        }
+    }
+
 }
---
+++ real_estate-UWP/Sections/Ads/AdsView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace real_estate_UWP.Sections.Ads
+{
+    public sealed partial class AdsView : Page
+    {
+        public AdsView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (ContentContainer.ActualWidth < 601)
+            {
+                ContentArea.Width = ContentContainer.ActualWidth - ContentContainer.Padding.Right * 2;
+            }
+            else ContentArea.Width = 450;
+        }
+    }
+}
