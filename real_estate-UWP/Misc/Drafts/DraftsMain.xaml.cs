10e893c16fe372282ea4b9cb514c337dc7196a05
Dmitry Zhomir <giantdem@mail.ru>
2019-02-22 02:52:03 +0800

    Раздел «Черновики»

--- real_estate-UWP/Misc/Drafts/DraftsMain.xaml.cs
+++ real_estate-UWP/Misc/Drafts/DraftsMain.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -13,18 +14,69 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace real_estate_UWP.Misc.Drafts
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class DraftsMain : Page
     {
         public DraftsMain()
         {
             this.InitializeComponent();
+
+            this.ViewModel = new DraftsVM();
+        }
+
+        public DraftsVM ViewModel { get; set; }
+
+        private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (ContentContainer.ActualWidth < 601)
+            {
+                ContentArea.Width = ContentContainer.ActualWidth - ContentContainer.Padding.Right * 2;
+            }
+            else ContentArea.Width = 450;
+        }
+
+        private void listViewe4ka_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            this.Frame.Navigate(typeof(DraftsView));
+        }
+
+        private void filter_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(DraftsFilter));
         }
     }
+
+    public class DraftItem
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public DraftItem()
+        {
+            this.Title = "Имя записи";
+            this.Description = "Краткая информация";
+        }
+    }
+
+    public class DraftsVM
+    {
+        public DraftItem DefaultTestItem { get; } = new DraftItem();
+        public ObservableCollection<DraftItem> Drafts { get; } = new ObservableCollection<DraftItem>();
+
+        public DraftsVM()
+        {
+            this.Drafts.Add(new DraftItem()
+            {
+                Title = "Контракт 00001",
+                Description = "Аренда"
+            });
+            this.Drafts.Add(new DraftItem()
+            {
+                Title = "Объект 1",
+                Description = "Квартира"
+            });
+        }
+    }
+
 }
