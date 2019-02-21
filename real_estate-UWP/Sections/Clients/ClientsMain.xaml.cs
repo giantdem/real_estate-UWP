231808e41fbbe8f4dab712179cc01a03ac67f717
Dmitry Zhomir <giantdem@mail.ru>
2019-02-21 23:31:18 +0800

    Раздел «Клиенты»

--- real_estate-UWP/Sections/Clients/ClientsMain.xaml.cs
+++ real_estate-UWP/Sections/Clients/ClientsMain.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -13,18 +14,74 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace real_estate_UWP.Sections.Clients
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class ClientsMain : Page
     {
         public ClientsMain()
         {
             this.InitializeComponent();
+
+            this.ViewModel = new ClientsVM();
+        }
+
+        public ClientsVM ViewModel { get; set; }
+
+        private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (ContentContainer.ActualWidth < 601)
+            {
+                ContentArea.Width = ContentContainer.ActualWidth - ContentContainer.Padding.Right * 2;
+            }
+            else ContentArea.Width = 450;
+        }
+
+        private void listViewe4ka_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            this.Frame.Navigate(typeof(ClientsView));
+        }
+
+        private void filter_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(ClientsFilter));
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(ClientsAdd));
         }
     }
+
+    public class ClientItem
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public ClientItem()
+        {
+            this.Title = "Имя записи";
+            this.Description = "Краткая информация";
+        }
+    }
+
+    public class ClientsVM
+    {
+        public ClientItem DefaultTestItem { get; } = new ClientItem();
+        public ObservableCollection<ClientItem> Clients { get; } = new ObservableCollection<ClientItem>();
+
+        public ClientsVM()
+        {
+            this.Clients.Add(new ClientItem()
+            {
+                Title = "Клиент Клиентов",
+                Description = "Физическое лицо"
+            });
+            this.Clients.Add(new ClientItem()
+            {
+                Title = "ООО «Зеленоглазое такси»",
+                Description = "Юридическое лицо"
+            });
+        }
+    }
+
 }
